2ac70189629cbddac2fa30235e6da84a07ada23b
hsisco <hysensisco@gmail.com>
2020-07-23 17:16:32 -0400

    Sliding Window MVP

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,11 +2,11 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
-
+def sliding_window_max(arr, k):      
+    max_arr = []                        # create new array
+    for i in range(len(arr) - k + 1):   # for the provided array without the k values (+ 1 to make it accurate)
+        max_arr.append(max(arr[i:i+k])) # add to new array all values, skipping the k values in the span
+    return max_arr                      # see what's in the new array
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
